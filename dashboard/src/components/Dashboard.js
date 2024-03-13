bd0bb3982b2c027f7cce0f75babc481cce5d1ebd
codewithaquib <aquib@tabdeel.in>
2024-03-14 00:51:27 +0530

    Adding data to holdings and positions

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -9,6 +9,7 @@ import Orders from "./Orders";
 import Positions from "./Positions";
 import Summary from "./Summary";
 import WatchList from "./WatchList";
+import { GeneralContextProvider } from "./GeneralContext";
 
 const Dashboard = () => {
   return (
